5d940eacc3c60b3a567f0c27f6a4364781bd3add
Egor <sorokin.egorka@gmail.com>
2023-08-22 22:36:29 +0300

    1)Редактирование классов

--- Class player/Program.cs
+++ Class player/Program.cs
@@ -7,33 +7,34 @@ namespace Class_player
         static void Main(string[] args)
         {
             Renderer renderer = new Renderer();
-            Player player = new Player(5, 5);
+            Player player = new Player(5, 5, '@');
 
-            renderer.Draw(player.X, player.Y);
+            renderer.Draw(player.PositionX, player.PositionY, player.Symbol);
         }
     }
 
     class Renderer
     {
-        public void Draw(int x, int y, char charter = '@')
+        public void Draw(int positionX, int positionY, char symbol)
         {
             Console.CursorVisible = false;
-            Console.SetCursorPosition(x, y);
-            Console.Write(charter);
+            Console.SetCursorPosition(positionX, positionY);
+            Console.Write(symbol);
             Console.ReadKey(true);
         }
     }
 
     class Player
     {
-        public int X { get; private set; }
-        public int Y { get; private set; }
-
-        public Player(int x, int y)
+        public Player(int positionX, int positionY, char symbol)
         {
-            X = x;
-            Y = y;
+            PositionX = positionX;
+            PositionY = positionY;
+            Symbol = symbol;
         }
 
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+        public char Symbol { get; private set; }
     }
 }
