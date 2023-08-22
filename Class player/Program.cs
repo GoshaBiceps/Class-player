fa4083113c313cc60ca7897ec067c5622941f31d
Egor <sorokin.egorka@gmail.com>
2023-08-22 14:24:51 +0300

    Добавьте файлы проекта.

---
+++ Class player/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Class_player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Renderer renderer = new Renderer();
+            Player player = new Player(5, 5);
+
+            renderer.Draw(player.X, player.Y);
+        }
+    }
+
+    class Renderer
+    {
+        public void Draw(int x, int y, char charter = '@')
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(x, y);
+            Console.Write(charter);
+            Console.ReadKey(true);
+        }
+    }
+
+    class Player
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
